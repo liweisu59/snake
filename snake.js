a872050d322b3762ba772352881e0fd1fe20dedc
Liwei Su <liweisu59@outlook.com>
2025-05-15 11:40:27 +0800

    draw snake

---
+++ snake.js
@@ -0,0 +1,43 @@
+/** CONSTANTS **/
+const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BACKGROUND_COLOUR = 'white';
+
+// Get the canvas element
+var gameCanvas = document.getElementById('gameCanvas');
+
+// Return a two-dimensional drawing context
+var ctx = gameCanvas.getContext('2d');
+
+// Select the color to fill the canvas
+ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+// Select the color to fill with border of the canvas
+ctx.strokeStyle = CANVAS_BORDER_COLOUR;
+
+// Draw a "filled" rectangle to cover the entire canvas
+ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+// Draw a "border" around the entire canvas
+ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+// Snake representing
+let snake = [
+    {x: 150, y: 150},
+    {x: 140, y: 150},
+    {x: 130, y: 150},
+    {x: 120, y: 150},
+    {x: 110, y: 150},
+];
+
+// Creating and drawing snake
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokeStyle = 'darkgreen';
+
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+drawSnake();
